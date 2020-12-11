21d3141d49409305b3e4e3bec0f77b0d57993e32
gabriel-piedade95 <45984859+gabriel-piedade95@users.noreply.github.com>
2020-12-11 13:49:31 -0300

    adicionando listaLigada.h

---
+++ listaLigada.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct cel{
+	int linha;
+	struct cel * proximo;
+} celula;
+
+int vazia(celula * inicio);
+void imprimeLista (celula * inicio);
+celula * criaListaLigada();
+void insereFim(celula * inicio, int n);
+void insereInicio(celula ** inicio, int n);
+void inserePos(celula ** inicio, int pos, int n);
+celula * removeInicio(celula * inicio);
+celula * removeFim(celula * inicio);
+celula * removeCelula(celula * inicio, int n);
+int tamanhoLista(celula * inicio);
+celula * busca(celula * inicio, int n);
+void inserePos(celula ** inicio, int pos, int n);
+void inverteLista(celula ** inicio);
+celula * inverte(celula * inicio);
